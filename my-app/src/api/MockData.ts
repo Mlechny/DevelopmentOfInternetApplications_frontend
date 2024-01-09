b09f1425e083a570333c548def5005f086c0dc84
Dmitriy <rench.03@mail.ru>
2024-01-09 17:38:23 +0300

    correct mock-data

--- my-app/src/api/MockData.ts
+++ my-app/src/api/MockData.ts
@@ -5,29 +5,29 @@ export let languages = new Map<string, ILanguage>([
     ["a20163ce-7be5-46ec-a50f-a313476b2bd1",
         {
             uuid: "a20163ce-7be5-46ec-a50f-a313476b2bd1",
-            name: "CCCU6543210",
-            subject: "Стандартный 20-ти футовый контейнер",
+            name: "Python",
+            subject: "Парадигмы языков программирования",
             image_url: "http://localhost:9000/images/a20163ce-7be5-46ec-a50f-a313476b2bd1.jpg",
-            task: "Зерно",
-            description: "15000"
+            task: "Лабораторная работа 2",
+            description: "..."
         }
     ],
     ["8f157a95-dad1-43e0-9372-93b51de06163", {
         uuid: "8f157a95-dad1-43e0-9372-93b51de06163",
-            name: "CCCU6543210",
-            subject: "Стандартный 20-ти футовый контейнер",
+            name: "Swift",
+            subject: "IOS-спринт",
             image_url: "http://localhost:9000/images/8f157a95-dad1-43e0-9372-93b51de06163.jpg",
-            task: "Зерно",
-            description: "15000"
+            task: "ДЗ",
+            description: "..."
     }
     ],
     ["07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2", {
         uuid: "07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2",
-            name: "CCCU6543210",
-            subject: "Стандартный 20-ти футовый контейнер",
+            name: "JavaScript",
+            subject: "Программирование сетевых приложений",
             image_url: "http://localhost:9000/images/07d0cbdc-8e0f-4308-a7aa-11976ee6e5b2.jpg",
-            task: "Зерно",
-            description: "15000"
+            task: "Дополнительное задание",
+            description: "..."
     }
     ]
 ])
